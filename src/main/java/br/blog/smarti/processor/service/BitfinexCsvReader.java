088d9a7fe00473612c99f8e21a6382b50efc6703
Paulo Cezar <paulocezar@brq.com>
2022-11-17 17:10:43 -0300

    - fixing log to v2

--- src/main/java/br/blog/smarti/processor/service/BitfinexCsvReader.java
+++ src/main/java/br/blog/smarti/processor/service/BitfinexCsvReader.java
@@ -2,19 +2,18 @@ package br.blog.smarti.processor.service;
 
 import br.blog.smarti.processor.entity.BitfinexTrade;
 import br.blog.smarti.processor.enums.ExchangesEnum;
-import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.String.format;
 
-@Log4j
+@Log4j2
 @Service
 public class BitfinexCsvReader extends CsvReader {
 
