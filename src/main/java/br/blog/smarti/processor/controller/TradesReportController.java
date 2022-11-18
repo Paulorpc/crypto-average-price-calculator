156306d43475ed1b002f05cad067d2c2af62ef4f
Paulo Cezar <paulocezar@brq.com>
2022-11-17 23:34:29 -0300

    - implemented controller

--- src/main/java/br/blog/smarti/processor/controller/TradesReportController.java
+++ src/main/java/br/blog/smarti/processor/controller/TradesReportController.java
@@ -1,6 +1,30 @@
 package br.blog.smarti.processor.controller;
 
+import br.blog.smarti.processor.entity.ReportOutputTrade;
+import br.blog.smarti.processor.enums.ExchangesEnum;
+import br.blog.smarti.processor.service.ReportTradeService;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+@Log4j2
+@Controller
 public class TradesReportController {
 
+    @Autowired
+    private ReportTradeService reportTradeService;
+
+    /***
+     * Process all reports exchange file from input folder and generate a standard csv output report with all exchanges trades. 
+     * @param filesPath
+     * @throws FileNotFoundException
+     */
+    public void processTradesReports(String filesPath) throws Exception {
+        List<ReportOutputTrade> trades = reportTradeService.generateReportOutputTradeContent(filesPath, ExchangesEnum.BINANCE, ExchangesEnum.BITFINEX);
+        reportTradeService.generateReportOutputTradeCsv(filesPath, trades);
+    }
 
 }
--- src/main/java/br/blog/smarti/processor/service/CsvReader.java
+++ src/main/java/br/blog/smarti/processor/service/CsvReader.java
@@ -2,12 +2,14 @@ package br.blog.smarti.processor.service;
 
 import br.blog.smarti.processor.entity.Trade;
 import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Log4j2
 public abstract class CsvReader<T extends Trade> {
     
     private static final int HEADER = 1;
@@ -20,6 +22,8 @@ public abstract class CsvReader<T extends Trade> {
                 .build()
                 .parse();
 
+        log.debug(tuples.toString());
+        
         return tuples;
     }
 
